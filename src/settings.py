a9a237372b49fd5c7e3e2bcc9100274f047c5eb5
KhazaMohammad <khaza.md@gmail.com>
2025-07-22 18:40:33 +0530

    adding docker build

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,7 @@
 class Settings:
     def __init__(self):
-        self.kafka_bootstrap_servers = "localhost:9092"
+        #self.kafka_bootstrap_servers = "localhost:9092"
+        self.kafka_bootstrap_servers = "host.docker.internal:9092"
         self.kafka_security_protocol = "PLAINTEXT"
         self.topic = "code_check_01"
         self.api_key = "e825daf14c584975b38124833253004"
